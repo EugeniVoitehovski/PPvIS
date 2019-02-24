b0a7961d3ac1bc83726c8c82f03bec9dd5be219a
EugeniVoitehovski <eugeni@MacBook-Pro-EugeniVoitehovski.local>
2019-02-24 21:36:57 +0300

    button

--- PPvIS1/src/ppvis1/PPvIS1.java
+++ PPvIS1/src/ppvis1/PPvIS1.java
@@ -6,12 +6,14 @@
 package ppvis1;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 /**
  *
@@ -20,32 +22,46 @@ import javafx.stage.Stage;
 public class PPvIS1 extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+    public void start(Stage primaryStage) throws Expection {
+        Label selectedLB1 = new Label();
+        Button selectBtn = new Button("select");
+        
+        RadioButton aBtn = new RadioButton("111");
+        RadioButton bBtn = new RadioButton("222");
+        RadioButton cBtn = new RadioButton("333");
+        
+        ToggleGroup group = new ToggleGroup();
+        
+        aBtn.setToggleGroup(group);
+        bBtn.setToggleGroup(group);
+        cBtn.setToggleGroup(group);
+        
+        selectBtn.setOnAction(event -> {
+            RadioButton selection = (RadioButton) group.getSelectedToggle();
+            selectedLB1.setText("selected: "+selection.getText());
             
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10);
+        root.getChildren().addAll(aBtn, bBtn, cBtn, selectBtn, selectedLB1);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(root, 250, 200);
         primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+        primaryStage.setTitle("Buttons");
+        primaryStage.show();   
+      
+    }  
+       
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
+   
+
+   
+    
